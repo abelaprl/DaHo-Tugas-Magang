c15dc82f76f496246b9b5708e50850b3a5c77296
Abel Apriliani <123937217+abelaprl@users.noreply.github.com>
2023-05-23 08:41:01 +0700

    Update publisher_client.cpp

--- src/publisher_client.cpp
+++ src/publisher_client.cpp
@@ -5,7 +5,31 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "publisher_client");
     ros::NodeHandle nh;
 
-    
+    ros::ServiceClient client = nh.serviceClient<publisher_subscriber_server_client::Service>("nama_server");
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("nama_topic", 1000);
 
+    publisher_subscriber_server_client::Service srv;
+    srv.request.A = atoll(argv[1]);
+    srv.request.B = atoll(argv[2]);
+
+    ros::Rate loop_rate(10);
+
+    ros::Rate loop_rate(10);
+
+    if (client.call(srv)){
+        while (ros::ok()){
+            std_msgs::String msg;
+            msg.pesan = srv.response.Number;
+            chatter_pub.publish(msg);
+
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+    }
+    else{
+        ROS_ERROR("Failed");
+        return 1;
+    }
     return 0;
 }
+
