eef5b28ed81ad4621cba584f5a608b46b880f5d7
Abel Apriliani <123937217+abelaprl@users.noreply.github.com>
2023-05-23 08:46:21 +0700

    Update subscriber.cpp

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -2,7 +2,7 @@
 
 void callback(const publisher_subscriber_server_client::Message::ConstPtr& msg)
 {
-  
+  ROS_INFO('Hasil penjumlahan: [%s]', msg->pesan.c_str())
 }
 
 int main(int argc, char **argv)
@@ -10,7 +10,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "subscriber");
   ros::NodeHandle nh;
 
-  
+  ros::Subscriber sub = n.subscribe("nama_topic", 1000, callback);
+  ros::spin();
 
   return 0;
 }
