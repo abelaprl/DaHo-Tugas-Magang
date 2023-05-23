ac43430cafeb11c32b5dc920858fc2021b213faa
Abel Apriliani <123937217+abelaprl@users.noreply.github.com>
2023-05-23 08:45:24 +0700

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -3,7 +3,10 @@
 bool process(publisher_subscriber_server_client::Service::Request  &req,
              publisher_subscriber_server_client::Service::Response &res)
 {
-    
+    res.Number = req A + req B;
+    ROS_INFO("request: x=%ld, y=%ld", (long int)req.A, (long int)req.B);
+    ROS_INFO("sending back response: [%ld]", (long int)res.Number);
+    return true;
 }
 
 int main(int argc, char** argv)
@@ -11,7 +14,9 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "server");
     ros::NodeHandle nh;
 
-    
+    ros::ServiceServer service = nh.advertiseService("nama_server", process);
+    ROS_INFO("Ready to add two ints.");
+    ros::spin();
 
     return 0;
 }
